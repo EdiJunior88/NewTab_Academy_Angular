e625c665456051e41528b2e038b705664d1e65f5
EdiJunior88 <er.moura88@yahoo.com.br>
2022-05-27 22:33:03 -0300

    *ngFor

---
+++ Capitulo_14/curso/src/app/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  is_enable_menu = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

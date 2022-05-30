15fa9a792ec1a67f8339461b182aeb6447750d07
EdiJunior88 <er.moura88@yahoo.com.br>
2022-05-30 10:19:24 -0300

    Roteamento

---
+++ Capitulo_23/curso/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { SqrtPipe } from './sqrt.pipe';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    UsuariosComponent,
+    SqrtPipe,
+    HomeComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
